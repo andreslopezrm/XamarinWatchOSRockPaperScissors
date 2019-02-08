9f79538ee6b18620cc1e0237cd781f1162a1f404
Andres <andres_lopez_rm@hotmail.com>
2019-02-08 09:44:22 -0600

    inital commit

---
+++ RockPaperScissorsXamarin.RockPaperScissorsWatchExtension/InterfaceController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+using WatchKit;
+using UIKit;
+using Foundation;
+
+namespace RockPaperScissorsXamarin.RockPaperScissorsWatchExtension
+{
+    public partial class InterfaceController : WKInterfaceController
+    {
+        private string[] Options = { "rock", "paper", "scissors" };
+        private int Counter = 0;
+
+        protected InterfaceController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override void Awake(NSObject context)
+        {
+            base.Awake(context);
+
+            RockButton.SetBackgroundImage(new UIImage("rock"));
+            PaperButton.SetBackgroundImage(new UIImage("paper"));
+            ScissorsButton.SetBackgroundImage(new UIImage("scissors"));
+
+            Timer.Start();
+        }
+
+        public override void WillActivate()
+        {
+            Console.WriteLine("{0} will activate", this);
+        }
+
+        public override void DidDeactivate()
+        {
+            Console.WriteLine("{0} did deactivate", this);
+        }
+
+        partial void RockButton_Activated()
+        {
+            Check("rock");
+        }
+
+        partial void PaperButton_Activated()
+        {
+            Check("paper");
+        }
+
+        partial void ScissorsButton_Activated()
+        {
+            Check("scissors");
+        }
+
+        private void Check(string selection) 
+        {
+            var random = new Random();
+
+            var position = random.Next(0, Options.Length);
+            var machineChoose = Options[position];
+
+
+            if (selection == machineChoose)
+            {
+                ResultLabel.SetText("TIE");
+            }
+            else if (selection == "rock" && machineChoose == "scissors")
+            {
+                ResultLabel.SetText("WIN");
+                Counter += 1;
+            }
+            else if (selection == "rock" && machineChoose == "paper")
+            {
+                ResultLabel.SetText("LOSE");
+            }
+            else if (selection == "paper" && machineChoose == "rock")
+            {
+                ResultLabel.SetText("WIN");
+                Counter += 1;
+            }
+            else if (selection == "paper" && machineChoose == "scissors")
+            {
+                ResultLabel.SetText("LOSE");
+          }
+            else if (selection == "scissors" && machineChoose == "paper")
+            {
+                ResultLabel.SetText("WIN");
+                Counter += 1;
+          }
+            else if (selection == "scissors" && machineChoose == "rock")
+            {
+                ResultLabel.SetText("LOSE");
+          }
+
+            WinnerLabel.SetText($"Wins: {Counter}");
+            OponentLabel.SetText($"Oponent: { machineChoose }");
+
+        }
+    }
+
+}
---
+++ RockPaperScissorsXamarin.RockPaperScissorsWatchExtension/InterfaceController.designer.cs
@@ -0,0 +1,94 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace RockPaperScissorsXamarin.RockPaperScissorsWatchExtension
+{
+    [Register ("InterfaceController")]
+    partial class InterfaceController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceLabel OponentLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton PaperButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceLabel ResultLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton RockButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceButton ScissorsButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceTimer Timer { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceLabel WinnerLabel { get; set; }
+
+        [Action ("PaperButton_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void PaperButton_Activated ();
+
+        [Action ("RockButton_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void RockButton_Activated ();
+
+        [Action ("ScissorsButton_Activated")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void ScissorsButton_Activated ();
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (OponentLabel != null) {
+                OponentLabel.Dispose ();
+                OponentLabel = null;
+            }
+
+            if (PaperButton != null) {
+                PaperButton.Dispose ();
+                PaperButton = null;
+            }
+
+            if (ResultLabel != null) {
+                ResultLabel.Dispose ();
+                ResultLabel = null;
+            }
+
+            if (RockButton != null) {
+                RockButton.Dispose ();
+                RockButton = null;
+            }
+
+            if (ScissorsButton != null) {
+                ScissorsButton.Dispose ();
+                ScissorsButton = null;
+            }
+
+            if (Timer != null) {
+                Timer.Dispose ();
+                Timer = null;
+            }
+
+            if (WinnerLabel != null) {
+                WinnerLabel.Dispose ();
+                WinnerLabel = null;
+            }
+        }
+    }
+}
